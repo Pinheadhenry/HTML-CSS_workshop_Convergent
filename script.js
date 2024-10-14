c01b177bc47ae91f806778b12b74df133cc3c8f0
Henry Tudor <henrytudor2005@yahoo.com>
2024-10-14 11:25:12 -0500

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 // script.js
 
 // Alert message when the page loads
-window.onload = function() {
-  alert("Welcome to my first web page!");
-};
+// window.onload = function() {
+//   alert("Welcome to my first web page!");
+// };
 
 // Function to change the text when button is clicked
 function changeText() {
   document.getElementById("myParagraph").innerText = "You clicked the button!";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  console.log(slides)
+
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds
 }
